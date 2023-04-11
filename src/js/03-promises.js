2de0652048e3b280a2897ffd8e366976b61ebec6
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-04-11 13:50:15 +0300

    update task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formElement = document.querySelector('.form');
+
+// Set event listener submit on form
+formElement.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  let delay = Number(formElement.delay.value);
+
+  for (let i = 1; i <= formElement.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formElement.step.value);
   }
+}
+
+// Create promise
+function createPromise(position, delay) {
+  const obj = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(obj);
+      }
+      reject(obj);
+    }, delay);
+  });
 }
